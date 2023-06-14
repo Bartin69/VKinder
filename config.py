a0b2f10a67df6c9dd0a57d7373b7e8a46dd7be88
Bartin69 <108576511+Bartin69@users.noreply.github.com>
2023-06-14 17:31:31 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+user_token = 'vk1.a.Lk6zPph1PS7FOmAHQCzeXHJOQPJngme3BFnkkAkKH-YA7r6ibicnwd20orRmtxPf9RuMZjGwLZOavTeHH5F8mx1fwgx7tbGfz8GbqwF4o5-DE5pY4o0AaECaJDMCHDW-q3JnkeseQf9ePifKNl0I_hxxGmMpJxeDLla08IAdKp0ygQCe6LAXNMzJjr7RDlCCS2ZIMF00cqv-gChGjcwgMA' # токен пользователя
+comm_token = 'vk1.a.TTynY97lLVbfSs_NYG-t_VNZeLJ-uMLbeJt9TY1WF_OdgIuPy1sflt4ENaaQsui1Hvt0yvpqIu4ARidS-H73KRMcsRfy80quUIRRts2qFRf_am1PcFTylDiU3BsseFZnhPMpYWx79SE6P02FWNaNHfvbuxSk6HXMEyoLaCYm6HC6f6d6OJ0Yx0fnfxu4krcUFd9CBIamYAujD_JR0gewHg' # токен сообщества
+
+offset = 0
+line = range(0, 1000)
+
+host = '127.0.0.1' 
+user = 'postgres'
+password = 'Aviamodelist69'
+db_name = 'diplom'
